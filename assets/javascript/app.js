476e5f27e592265aa2469f54dbf093476aa3abf8
Christopher Kim <christopherkim@dhcp-v213-015.vp.reshsg.uci.edu>
2017-07-21 02:05:44 -0700

    basic logic and structure is done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,8 +4,7 @@
 
 var gifTastic = {
 
-	buttonArray: ['dogs', 'cats', 'birds'],
-	searchTerm: "",
+	buttonArray: ['dogs', 'cats', 'birds', 'lol', 'fail', 'laugh', 'basketball', 'poke', 'prank', 'jump'],
 
 	renderButtons: function() {
 
@@ -14,22 +13,83 @@ var gifTastic = {
 
 		//add buttons
 		for(var i=0; i<gifTastic.buttonArray.length; i++){
-
 			var newBtn = $('<button>');
 			newBtn.addClass('btn');
 			newBtn.addClass('queryBtn');
 			newBtn.attr('data-value', gifTastic.buttonArray[i]);
 			newBtn.text(gifTastic.buttonArray[i]);
 			$('#btnContainer').append(newBtn);
-
 		}
 	},
 
+	displayInfo: function (e) {
 
+		$('#displayGIFs').empty();
+		e.preventDefault();
+
+		var value = $(this).attr('data-value');
+		console.log(value);
+      	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + value + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+      	$.ajax({
+      		url: queryURL,
+      		method: 'GET'
+      	}).done(function(event){
+      		
+      		var results = event.data;
+      		console.log(results);
+
+      		for(var i=0; i<results.length; i++){
+
+      			var imageDiv = $('<div>');
+      			imageDiv.addClass('gifItems');
+
+      			var animateLink = results[i].images.fixed_height.url;
+      			var stillLink = results[i].images.fixed_height_still.url;
+
+      			//append rating
+      			var p = $('<p>').text("Rating: " + results[i].rating);
+
+      			var gifImage = $('<img>');
+
+      			gifImage.addClass('gifs');
+      			gifImage.attr('data-animate', animateLink);
+      			gifImage.attr('data-still', stillLink);
+      			gifImage.attr('data-state', 'still');
+      			gifImage.attr('src', stillLink);
+      			
+      			imageDiv.append(p);
+      			imageDiv.append(gifImage);
+
+      			$('#displayGIFs').append(imageDiv);
+      		}
+
+      		gifTastic.pauseGif();
+      	});
+	},
+
+	pauseGif: function () {
+		$('.gifs').on('click', function(){
+
+			//grab current state of gif
+			var state = $(this).attr('data-state');
+
+			if(state === 'still'){
+
+				$(this).attr('src', $(this).attr('data-animate') );
+        		$(this).attr('data-state', 'animate');
+			} else {
+
+				$(this).attr('src', $(this).attr('data-still') );
+        		$(this).attr('data-state', 'still');
+			}
+			
+		})
+	}
 }
 
 
-$(document).ready(function (){
+window.onload = function () {
 
 	gifTastic.renderButtons();
 
@@ -50,7 +110,7 @@ $(document).ready(function (){
 		gifTastic.renderButtons();
 	});
 
-	$('.queryBtn').on('click', function() {
-		console.log("queryBtn is clicked");
-	});
-});
+	// $('.queryBtn').on('click', gifTastic.displayInfo);
+	$(document).on('click', '.queryBtn', gifTastic.displayInfo);
+
+}
